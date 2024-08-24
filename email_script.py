93acf6b37d1425ede759142c217f55dc73305bea
ehabejjeh <ehabejjeh@gmail.com>
2024-08-24 18:02:10 +0300

    updated email sending function
    included a html file that contain variables as the body of the message

--- email_script.py
+++ email_script.py
@@ -1,15 +1,20 @@
 import smtplib
 from email.message import EmailMessage
-
+from string import Template
+from pathlib import Path
 
 def send_email():
+
+    html = Template(Path("index.html").read_text())
+    substitute = html.substitute({"name": "Sally", "amount" : "500000$"})
+
     msg = EmailMessage()
     sender = 'ihab.alajeh@gmail.com'
     msg['From'] = "Ihab Alajeh"
     msg['To'] = 'ehabejjeh@gmail.com'
     msg['Subject'] = 'Greetings'
 
-    msg.set_content('Dear Sir,\n my name is Ihab, and this is a test email.')
+    msg.set_content(substitute, "html")
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
 
         server.login(sender, 'czynkbtfbsfceydr')
